c4967b6d169bd8e4bc93d29153f8c172096b7c3d
Philippe Loctaux <p@philippeloctaux.com>
2020-08-08 11:38:42 +0200

    added epitok and signin page works

--- src/auth.rs
+++ src/auth.rs
@@ -1,13 +1,39 @@
 use actix_web::{web, HttpResponse, Responder};
+use epitok::auth::Auth;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
-pub struct SigninData {
+pub struct FormData {
     autologin: String,
 }
 
-pub async fn sign_in(form: web::Form<SigninData>) -> impl Responder {
+pub async fn sign_in(form: web::Form<FormData>) -> impl Responder {
+    let mut auth = Auth::new();
+    match auth.sign_in(&form.autologin).await {
+        Ok(()) => (),
+        Err(e) => {
+            return HttpResponse::Forbidden()
+                .content_type("text/html")
+                .body(e.to_string())
+        }
+    };
+    let autologin = match auth.autologin() {
+        Some(autologin) => autologin,
+        None => {
+            return HttpResponse::InternalServerError()
+                .content_type("text/html")
+                .body("oops wtf")
+        }
+    };
+    let login = match auth.login() {
+        Some(login) => login,
+        None => {
+            return HttpResponse::InternalServerError()
+                .content_type("text/html")
+                .body("oops wtf")
+        }
+    };
     HttpResponse::Ok()
         .content_type("text/html")
-        .body(format!("got {}", form.autologin))
+        .body(format!("autologin {}, login {}", autologin, login))
 }
